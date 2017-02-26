266bce7b8c62424e6585a47f5be93e1d2beb9177
Adam Jahur <adamnjahur@gmail.com>
2017-02-25 20:41:14 -0500

    set up the connection to db, errors for db connection and required both models js files

--- server.js
+++ server.js
@@ -11,3 +11,26 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(express.static('public'));
+
+var database = {
+
+	local : 'mongodb://localhost/...'
+	remote : 'mongodb://...'
+}
+
+var whichDb = database.remote;
+mongoose.connect(whichDb);
+
+db = mongoose.connection;
+
+db.on('error', function (err) {
+	console.log('Mongoose Error: ', err);
+});
+db.once('open', function () {
+	console.log('DB connection: ',whichDb);
+});
+var PORT = process.env.PORT || 8080;
+
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js');
+
