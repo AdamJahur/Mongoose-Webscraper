bde1a31596281b714e5c4058871eb17c0b41322a
Adam Jahur <adamnjahur@gmail.com>
2017-02-25 20:54:02 -0500

    finished app.get for cnn scrape

--- server.js
+++ server.js
@@ -34,3 +34,34 @@ var PORT = process.env.PORT || 8080;
 var Note = require('./models/Note.js');
 var Article = require('./models/Article.js');
 
+app.get('/scrape', function(req, res){
+	request('http://www.cnn.com/', function(error, response, html){
+		var $ = cheerio.load(html);
+
+			$('article h3').each(function(i, element){
+				var result = {};
+				result.title = $(this).find('span').text();
+				result.link = $(this).children('a').attr('href');
+				//some CNN links already have http to an outside site otherwise they are CNN links.
+				if(result.link.indexOf("http")<0){
+					result.link = 'http://www.cnn.com' + result.link;
+				}
+				var entry = new Article (result);
+				entry.save(function(err, doc){
+					if (err) {
+						console.log(err);
+					}
+				})
+			})
+
+			Article.find({}, function(err, doc){
+				if (err) {
+					console.log(err);
+				} else {
+					res.json(doc);
+				}
+			})
+	})
+});
+
+
