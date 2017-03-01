dff89fce6faa102048d5c579cbe16d7402f9964c
Adam Jahur <adamnjahur@gmail.com>
2017-03-01 12:02:13 -0500

    trying to make mlab to work

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/mongoose-webscraper")
+var database = {
+	local: 'mongodb://localhost/mongoose-webscraper',
+	remote: 'mongodb://heroku_d0qvc6kc7kdvpb2be2i4373ng8e7v@ds161039.mlab.com:61039/heroku_d0qvc6xw'
+}
+
+var whichDb = database.remote;
+mongoose.connect(whichDb);
 
 db.on('error', function (err) {
 	console.log('Mongoose Error: ', err);
